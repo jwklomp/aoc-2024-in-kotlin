e306760bb8fff17d3f9a1a4530b8f46b6f90b9d1
jwklomp <jan-willem.klomp@visma.com>
2024-12-03 08:30:47 +0100

    Day 3

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,28 +1,55 @@
-fun main() {
-    fun extractMulMatches(input: String): List<Pair<Int, Int>> {
-        val regex = Regex("""mul\((\d{1,3}),(\d{1,3})\)""")
-        return regex.findAll(input).map { matchResult ->
-            val x = matchResult.groupValues[1].toInt()
-            val y = matchResult.groupValues[2].toInt()
-            x to y
-        }.toList()
-    }
+fun removeDeactivatedPortions(input: String): String {
+    var skip = false
+    var index = 0
 
+    return buildString {
+        while (index < input.length) {
+            when {
+                input.startsWith("don't()", index) -> {
+                    skip = true
+                    index += "don't()".length
+                }
+                input.startsWith("do()", index) -> {
+                    skip = false
+                    index += "do()".length
+                }
+                skip -> {
+                    index++
+                }
+                else -> {
+                    append(input[index])
+                    index++
+                }
+            }
+        }
+    }
+}
+
+fun extractMulMatches(input: String): List<Pair<Long, Long>> {
+    val regex = Regex("""mul\((\d{1,3}),(\d{1,3})\)""")
+    return regex.findAll(input).map { matchResult ->
+        val x = matchResult.groupValues[1].toLong()
+        val y = matchResult.groupValues[2].toLong()
+        x to y
+    }.toList()
+}
+
+fun main() {
 
     fun part1(input: List<String>): Long {
-        input.println()
-        return extractMulMatches(input[0]).sumOf { (x, y) -> (x * y).toLong() }
+        return input.sumOf { extractMulMatches(it).sumOf { (x, y) -> (x * y) } }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<String>): Long {
+        val sanitized = removeDeactivatedPortions(input.joinToString())
+        return extractMulMatches(sanitized).sumOf { (x, y) -> (x * y) }
     }
 
     val testInput = readInput("Day03_test")
     part1(testInput).println()
-    //part2(testInput).println()
+    part2(testInput).println()
 
     val input = readInput("Day03")
     part1(input).println()
-    //part2(input).println()
+    part2(input).println()
 }
